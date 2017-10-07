d8302bb2adc580382dff7ade5580c3e67db062e2
Kanitkorn S <k.sujautra@gmail.com>
2017-10-07 16:48:34 +0700

    Add type to functions in createRedisClient

--- src/createRedisClient.ts
+++ src/createRedisClient.ts
@@ -1,19 +1,19 @@
 import * as Redis from 'ioredis'
 
-function createRedisSentinelClient (sentinelUrls, sentinelMasterName) {
+function createRedisSentinelClient (sentinelUrls: { host: string, port: number }[], sentinelMasterName: string) {
   return new Redis({
     sentinels: sentinelUrls,
     name: sentinelMasterName
   })
 }
 
-function createRedisNormalClient (redisUrl) {
+function createRedisNormalClient (redisUrl: string) {
   return new Redis(redisUrl)
 }
 
-function formatRedisUrl (url) {
+function formatRedisUrl (url: string) {
   const [ host, port ] = url.split(':')
-  return { host, port }
+  return { host, port: parseInt(port, 10) }
 }
 
 export default function createRedisClient (redisUrl: string = 'redis://localhost:6379', sentinelMasterName: string = 'mymaster') {
@@ -26,5 +26,5 @@ export default function createRedisClient (redisUrl: string = 'redis://localhost
     .split(',')
     .map(url => formatRedisUrl(url))
 
-  return createRedisSentinelClient(sentinelUrls, sentinelMasterName)
+  return createRedisSentinelClient (sentinelUrls, sentinelMasterName)
 }
